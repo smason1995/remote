c5aecfaba19b2e3c3249b5042c8d7be9ed4d475f
Sydney Ann Mason <smason516@outlook.com>
2019-01-20 02:08:35 -0500

    Updated header file

--- code/dll.h
+++ code/dll.h
@@ -1,4 +1,30 @@
 #ifdef _DLL_H_
 #define _DLL_H_
 
+/***********
+ * Structs *
+ ***********/
+typedef struct node{
+    void *data;
+    int key;
+    struct *next;
+    struct *prev;
+} Node;
+
+/********************
+ * Global Variables *
+ ********************/
+extern Node *head;
+extern Node *last;
+extern Node *current;
+
+/*************
+ * Functions *
+ *************/
+extern void push(Node* link, void* val);
+extern void pop(Node* link);
+extern void* get(int key);
+extern Node* remove(int key);
+extern bool isEmpty();
+extern int length();
 #endif
